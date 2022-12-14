b9c5c4a163081c6e25c2e6fb7701a6ee27276a4e
Masoumeh <mrouhi20@gmail.com>
2022-12-13 22:29:45 -0800

    Initial commit

---
+++ src/test/java/com/pagefreezer/runner/CukesRunner.java
@@ -0,0 +1,17 @@
+package com.pagefreezer.runner;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        plugin = "html:target/cucumber-reports.html",
+        features = "src/test/resources/features",
+        glue = "com/pagefreezer/step_definitions",
+        dryRun = true,
+        tags = ""
+
+)
+public class CukesRunner {
+}
---
+++ src/test/resources/features/grammarly.feature
@@ -0,0 +1,4 @@
+Feature: Test if Grammarly Chrome Extension is installed and enabled
+  user story:
+
+  Scenario:
